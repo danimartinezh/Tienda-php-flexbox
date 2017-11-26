d6537700479f67c72ff9dcd2367685776ebfefc7
Dani-PC <dani.m.h@hotmail.com>
2017-11-26 01:15:30 +0100

    Dani
    TODO

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+echo '<header>
+  <div class="logo-menu">
+    <div class="container">
+      <div class="row middle-xs between-xs">
+        <div class="logo col-xs-12 col-sm-4 center-xs start-sm">
+          <img src="img/logo.png" alt="Peces Componentes">
+        </div>
+        <nav class="menu col-xs-12 col-sm-8 center-xs end-sm">
+          <a href="#">Inicio</a>
+          <a href="#">Iniciar Sesion</a>
+          <a href="#">Registrate</a>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div class="sub-menu">
+    <div class="container">
+      <div class="row middle-xs between-xs">
+        <nav class="categorias col-xs-12 center-xs">
+          <a href="#">Categoria 1</a>
+          <a href="#">Categoria 2</a>
+          <a href="#">Categoria 3</a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</header>';
+ ?>
--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <link rel="stylesheet" href="css/flexboxgrid.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Peces Componentes</title>
+  <!-- http://flexboxgrid.com/ --><!-- https://lenguajecss.com/p/css/propiedades/flexbox -->
+  <!-- http://htmlcolorcodes.com/es/tabla-de-colores/ -->
+  <!-- http://fontawesome.io/icons/ -->
+  <!-- https://icomoon.io/app/#/select -->
+  <!-- https://daneden.github.io/animate.css/ --> <!-- animated infinite nombre -->
 </head>
 <body>
-
+  <?php
+  include 'header.php'
+  ?>
 </body>
 </html>
